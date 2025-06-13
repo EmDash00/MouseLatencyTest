4b27fd9b0e19821906db765bccc8e8ccde0dc59c
Wispy Daydream <github.com.ounce967@passmail.net>
2025-06-13 16:25:28 -0700

    Optimize chromium

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
-import sys
+import textwrap
 import threading
 import time
 from collections import deque
@@ -13,21 +13,32 @@ import mss.tools
 import numpy as np
 from cv2.typing import MatLike
 from mss.base import MSSBase
-from playwright.__main__ import main as playwright_main
 from playwright.async_api import async_playwright
+from scipy.fft import rfft, rfftfreq
+
+from util import find_closest
 
 f0 = 0.1
 N = int(1 / f0) + 1
 w = np.arange(N) * f0 * tau
-dt = 0.001
 phi = np.linspace(0, 0.25 * tau, N)
+scale = 0.1 / 0.6904998132600674
+frame_count = 1
+PRELOADED_FRAMES = 20
+
 
 # Configuration
 FPS = 60
 DURATION_SEC = 3 / f0
 TOTAL_FRAMES = int(FPS * DURATION_SEC) + 1
-OUTPUT_FILE = "capture.mp4"
-FOURCC = cv2.VideoWriter_fourcc(*"mp4v")  # Use 'XVID' for AVI if needed
+
+t = np.arange(PRELOADED_FRAMES, TOTAL_FRAMES + PRELOADED_FRAMES) / FPS
+x = w[:, np.newaxis] * t + phi[:, np.newaxis]
+u = scale * np.sin(x).sum(axis=0) / N
+fft_freqs = rfftfreq(TOTAL_FRAMES, 1 / FPS)
+fi = find_closest(fft_freqs, f0 * np.arange(1, N))
+f = fft_freqs[fi]
+U = rfft(u)[fi]
 
 t0 = time.perf_counter()
 frame_count = 0
@@ -35,7 +46,7 @@ frames = deque()
 
 # Crop the region we record to reduce latency and processing work.
 WIDTH_CROP_RATIO = 0.2
-HEIGHT_CROP_RATIO = 0.1
+HEIGHT_CROP_RATIO = 0.05
 
 
 def process_frames(frames: deque[MatLike]):
@@ -50,12 +61,17 @@ def process_frames(frames: deque[MatLike]):
         center_px = M["m10"] / M["m00"]
         centers[i] = WIDTH_CROP_RATIO * (2 * center_px / width - 1)
 
+    Y = rfft(centers)[fi]
+    H_yu = Y / U  # type: ignore
+    H_yu_phase = np.angle(H_yu)
+    group_delay, b = np.polyfit(f * tau, H_yu_phase, deg=1)
+
+    r_squared = 1 - ((H_yu_phase - (group_delay * f * tau + b)) ** 2).sum() / (
+        H_yu_phase.var() * (H_yu_phase.size - 1)
+    )
+
+    print(f"Calculated group delay: {-group_delay:.3f} seconds (r^2 = {r_squared:.3f})")
     np.savetxt("capture.npy", centers)
-
-
-def ensure_browsers_installed():
-    sys.argv = ["", "install"]
-    playwright_main()
 
 
 def select_monitor(sct: MSSBase):
@@ -172,12 +188,47 @@ DEFAULT_BROWSER_ARGS = {
 }
 
 
+def select_browser() -> str:
+    selection_list = [
+        "chromium",
+        "webkit",
+        "firefox",
+    ]
+    while True:
+        print(
+            textwrap.dedent("""
+            Which browser type will you be playing on?
+            [1] Chromium (Edge, Chrome, Brave)
+            [2] Apple Webkit (Safari)
+            [3] Firefox
+            Your choice [1-3]: """),
+            end="",
+        )
+        selection = input()
+
+        if len(selection) != 1:
+            print("Selection should be a single digit.")
+            continue
+
+        if not selection[0].isdigit():
+            print("Selection should be a single digit.")
+            continue
+
+        index = int(selection)
+
+        if not (index >= 1 and index <= 3):
+            print("Selection should be a number [1-3].")
+            continue
+
+        return selection_list[index - 1]
+
+
 async def main():
     dt = deque()
     with mss.mss() as sct:
         async with async_playwright() as p:
             monitor = select_monitor(sct)
-            browser_type = "chromium"
+            browser_type = select_browser()
 
             browser = await {
                 "firefox": p.firefox,
@@ -203,13 +254,27 @@ async def main():
 
             # Create initial page
             page = await context.new_page()
-
             await page.goto("http://localhost:5000")
+            cdp_session = None
+
+            try:
+                cdp_session = await context.new_cdp_session(page)
+            except Exception:
+                pass
 
             async def move_mouse(x, y):
                 px = 0.5 * (viewport["width"] - 1) * (x + 1)
                 py = 0.5 * (viewport["height"] - 1) * (y + 1)
-                # await page.mouse.move(px, py)
+
+                if cdp_session is not None:
+                    await cdp_session.send('Input.dispatchMouseEvent', {
+                        'type': 'mouseMoved',
+                        'x': px,
+                        'y': py
+                    })
+                    return
+
+
                 await page.evaluate(
                     """([px, py]) => {
                      document.dispatchEvent(
@@ -227,13 +292,11 @@ async def main():
             SCREENSHOT_REGION = {
                 "top": int(monitor["top"] + monitor["height"] * 0.575),
                 "left": int(monitor["left"] + monitor["width"] * 0.405),
-                "height": int(monitor["height"] * 0.05),
-                "width": int(monitor["width"] * 0.2),
+                "height": int(monitor["height"] * HEIGHT_CROP_RATIO),
+                "width": int(monitor["width"] * WIDTH_CROP_RATIO),
             }
 
-            scale = 0.1 / 0.6904998132600674
             frame_count = 1
-            PRELOADED_FRAMES = 20
             t0 = time.perf_counter()
             try:
                 # Capture frames with precise timing
@@ -248,7 +311,6 @@ async def main():
 
                     while (time.perf_counter() - t0) < next_frame_time:
                         await asyncio.sleep(0)
-                        pass
 
                 t1 = 0
                 for _ in range(TOTAL_FRAMES):
@@ -273,16 +335,11 @@ async def main():
                         # This yields control back to the browser for one
                         # update cycle to allow it to continue working.
                         await asyncio.sleep(0)
-                        # pass
 
             finally:
-                # input()
-                # Cleanup resources
                 await browser.close()
-                print(f"Saved {TOTAL_FRAMES} grayscale frames to {OUTPUT_FILE}")
 
     process_frames(frames)
-    np.savetxt("dt.npy", dt)
 
 
 if __name__ == "__main__":
